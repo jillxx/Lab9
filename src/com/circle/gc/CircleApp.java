316b374b7659f064949f74062659e883596f4d10
jillxx <jiaoxu84@gmail.com>
2018-05-07 09:51:50 -0400

    adding count

--- src/com/circle/gc/CircleApp.java
+++ src/com/circle/gc/CircleApp.java
@@ -8,21 +8,23 @@ public class CircleApp {
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Welcome to the Circle Tester");
 		String cont = "y";
+		int num = 0;
 		while (cont.equalsIgnoreCase("y")) {
 			// System.out.print("Please enter a radius: ");
 			double radius = Validator.getDouble(scnr, "Enter a radius: ");
 			// double radius = scnr.nextDouble();
 
 			Circle c = new Circle(radius);
-
+ 
 			System.out.println("Circumference: " + c.getFormattedCircumference(radius));
 			System.out.println("Area: " + c.getFormattedArea(radius));
 
 			System.out.println("Continue? (y/n)");
 			cont = scnr.next();
-
+            num++;
 		}
 		System.out.println("Goodbye.");
+		System.out.println("You have calculated "+ num + " circles");
 		scnr.close();
 	}
 
